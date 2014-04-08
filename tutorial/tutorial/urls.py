d5a7ac18948e8bb0bee08d7ffe68cf65bd62b78e
geekofalltrades <thegeekofalltrades@gmail.com>
2014-04-08 09:32:54 -0700

    Added login urls to project urlconf.

--- tutorial/tutorial/urls.py
+++ tutorial/tutorial/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tutorial.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('snippets.urls')),
+)
+
+urlpatterns += patterns(
+    '',
+    url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 )
